eae41565b67d38cddca652487e75b3b89c0704cd
ricejf <rice_justin_f@cat.com>
2017-10-01 14:48:33 -0500

    Fix

--- Stockify.Api/HighPresence.aspx.cs
+++ Stockify.Api/HighPresence.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Stockify.Api
 {
-    public partial class StockChatter : System.Web.UI.Page
+    public partial class HighPresence : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
